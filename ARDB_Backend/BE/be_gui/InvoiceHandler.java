1994248b828b1fd1bf7e2ad38e1f4151610d61ce
jonathanedwardsgm <jonathan.edwards.gm@gmail.com>
2015-11-11 16:25:26 -0500

    Working on Invoicing Systems

--- ARDB_Backend/BE/be_gui/InvoiceHandler.java
+++ ARDB_Backend/BE/be_gui/InvoiceHandler.java
@@ -1,15 +1,95 @@
 package be_gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+import javax.swing.Timer;
+
+import utilities.FileEmailer;
 import utilities.InvoiceGenerator;
 
 public class InvoiceHandler extends ServicePanelInd {
 
+	private String invoiceFilePath;
+	private File currentInvoiceFile;
+	private long currentInvoiceTimeStamp;
+	private FileEmailer invoiceEmailer;
 	private InvoiceGenerator invGen;
-
+	private SimpleDateFormat sdf;
+	private InvoiceListener il;
+	
+	
 	public InvoiceHandler() {
 		super("Invoices");
+		sdf = new SimpleDateFormat("MM-dd-yy kk:mm");
+		invoiceEmailer = new FileEmailer();
+		il = new InvoiceListener();
+		timer = new Timer(27777, il);
 		invGen = new InvoiceGenerator();
-		invGen.pdfTester();
+		
+		invoiceFilePath = "Z:\\CCINV";
+		currentInvoiceFile = new File(invoiceFilePath);
+		currentInvoiceTimeStamp = currentInvoiceFile.lastModified();
+		
+		timer.start();
+		lblStatus.setText("Running");
+		lblLastUpdate.setText(sdf.format(new Date()));
+		
+	}
+	
+	private boolean parseInvoices(File file){
+		ArrayList<Invoice> invoices = new ArrayList<Invoice>();
+		ArrayList<Invoice> electronic = new ArrayList<Invoice>();
+		try {
+			Scanner scan = new Scanner(file);
+			scan.useDelimiter("BEGIN");
+			if(!scan.hasNext()){
+				return false;
+			}
+			scan.next();
+			while(scan.hasNext()){
+				Invoice i = new Invoice(scan.next());
+				invoices.add(i);				
+				if(i.isElectronic()){
+					electronic.add(i);
+				}
+			}
+			
+			processElectronicInvoices(electronic);
+					
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	private String processElectronicInvoices(ArrayList<Invoice> invoices){
+		
+		return null;
+	}
+	
+	
+	private class InvoiceListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			File tempFile = new File(invoiceFilePath);
+			long tempTimeStamp = tempFile.lastModified();
+			if(tempTimeStamp != currentInvoiceTimeStamp){
+				currentInvoiceFile = tempFile;
+				currentInvoiceTimeStamp = currentInvoiceFile.lastModified();
+				parseInvoices(currentInvoiceFile);
+			}
+			
+		}
+		
 	}
 
 }
