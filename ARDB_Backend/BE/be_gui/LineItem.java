8ed4f3689ddb8fb912b3f77857779609ebb892a1
jonathanedwardsgm <jonathan.edwards.gm@gmail.com>
2016-02-16 15:46:32 -0500

    adding specific qualifications to line item

--- ARDB_Backend/BE/be_gui/LineItem.java
+++ ARDB_Backend/BE/be_gui/LineItem.java
@@ -4,13 +4,59 @@ import java.text.DecimalFormat;
 
 public class LineItem {
 	
-		
-
-		
+	private int depCode;
+	private String prodCode;
+	private String procommText;
+	private String fullDescription;
+	private float width, length;
+	private String colorCode;
+	private String colorName;
+	private char inOutMount;
+	private char tilter;
+	private char lift;
+	private char valance;
+	private char holdDown;
+	private char stock;
+	private char wallCeilMount;
+	private String line;
 	
-	
-	public LineItem(String l) {
-		// TODO Auto-generated constructor stub
+	public LineItem(String item) {
+		line = item;
+		
+		try{
+			depCode = Integer.parseInt(item.substring(0,2));
+		} catch(NumberFormatException nfe){
+			depCode = -1;
+		}
+		
+		try{
+			prodCode = item.substring(2,11).trim();
+		} catch(StringIndexOutOfBoundsException siob){
+			prodCode = null;
+		}
+		
+		try{
+			procommText = item.substring(12,30);
+		} catch(StringIndexOutOfBoundsException siob){
+			procommText = "******";
+		}
+		
+		try{
+			width = Float.parseFloat(item.substring(32,38).trim());
+		} catch(NumberFormatException nfe){
+			width = -1;
+		}
+		
+		
+		try{
+			length = Float.parseFloat(item.substring(39,44).trim());
+		} catch(NumberFormatException nfe){
+			length = -1;
+		}
+		
+		
+		
+		
 	}
 
 	private String fixNumber(double n) {
