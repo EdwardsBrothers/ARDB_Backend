d8439ae9f3e8b5b9223092ec0fe9802013806aec
jedwards <jedwards@jlaptop-10.mmo.com>
2019-02-26 16:58:00 -0500

    Sales Email init

---
+++ ARDB_Backend/BE/simplified_applications/SalesEmailerApp.java
@@ -0,0 +1,43 @@
+package simplified_applications;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class SalesEmailerApp {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SalesEmailerApp window = new SalesEmailerApp();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public SalesEmailerApp() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ ARDB_Backend/BE/simplified_applications/SalesEmailerPanel.java
@@ -0,0 +1,14 @@
+package simplified_applications;
+
+import javax.swing.JPanel;
+
+public class SalesEmailerPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public SalesEmailerPanel() {
+
+	}
+
+}
